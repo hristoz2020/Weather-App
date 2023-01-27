e3c10736b4e8d9e177e87e432e0972376f0420bc
hristoz2020 <62563444+hristoz2020@users.noreply.github.com>
2023-01-27 23:49:50 +0200

    Initial commit

---
+++ src/api.js
@@ -0,0 +1,14 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = '8c6d3268d1386a56c11a0eb2cb7bf715';
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '5a8991bdb0msh3a45b3554513272p1a50f5jsn1f7e79eecad9',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+
